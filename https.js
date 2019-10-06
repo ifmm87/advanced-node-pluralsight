f2268fb7a7fb58b83e2c69a8f80ccfcfab145266
ivan mujica <ifmm87@gmail.com>
2019-10-06 18:14:05 -0400

    https

---
+++ https.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const server = require('https')
+  .createServer({
+    key: fs.readFileSync('./key.pem'),
+    cert: fs.readFileSync('./cert.pem')
+  });
+server.on('request', (req, res) => {
+  res.writeHead(200, {'content-type': 'text/plain'});
+  res.end('Hello world, Ivancho!');
+});
+server.listen(443);
