9f82881d619b0d8d751b479179195cf53a5616ea
Ivancho <ifmm87@gmail.com>
2019-10-06 01:37:12 -0400

    udp

--- dns/index.js
+++ dns/index.js
@@ -2,4 +2,11 @@ const dns = require('dns');
 dns.lookup('google.com', (err, address) => {
   console.log('==========addresssss==================================');
   console.log(address);
-})
+});
+
+dns.resolve4('pluralsight.com','MX', (err, address)=> {
+  console.log(address);
+});
+dns.reverse('35.161.75.227','MX', (err, address)=> {
+  console.log(address);
+});
